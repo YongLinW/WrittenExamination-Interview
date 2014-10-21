62cb8cdc0b9f92871dd3a3eda056d4efa71d7baa
LinYongW <linyongw@gmail.com>
2014-10-21 15:05:50 +0800

    add problem

---
+++ WrittenExamination-Interview/src/main/java/cn/campusRecruitment2014/IDG/WordShortestPath/WordShortestPath.java
@@ -0,0 +1,24 @@
+package cn.campusRecruitment2014.IDG.WordShortestPath;
+
+/**
+ * 
+ *@explanation 寻找单词的最短路径
+ *@author YongLin
+ *@email  yonglin@hengtiansoft.com
+ *@time   2014年10月21日下午2:50:39
+ */
+
+/*
+ * 该题为2014年IDG校园招聘，笔试题中的编程题二
+ * 给定首位单词以及一个词典，从词典中找出最短的路径使得从首单词到达尾单词，其中所有单词的位数相同（<20），词典中单词的数量小于100000
+ * 要求：从首单词开始，每次只能变换一位的字母，所有变化的单词都出现在给定的词典中，找出最短的路径，如果不存在路径则输出“No Way!”
+ * 举例：start="hot",end="dog",dictionary={"","","","","",""}
+ * 其中最短路径为{"","",""}
+ */
+public class WordShortestPath {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ WrittenExamination-Interview/src/main/java/cn/campusRecruitment2014/Netease/MaximumSubarray/MaximumSubarray.java
@@ -0,0 +1,26 @@
+package cn.campusRecruitment2014.Netease.MaximumSubarray;
+
+/**
+ * 
+ *@explanation 求解给定int型数组的最大子串 
+ *@author YongLin
+ *@email  yonglin@hengtiansoft.com
+ *@time   2014年10月20日下午3:04:06
+ */
+
+/*
+ * 本题目为2014年网易校园招聘面试算法编程题目
+ * 给定一个int型数组，其中的元素包含正数以及负数，求解其中连续元素子串中元素之和最大的子串
+ * 举例：array = {−2, 1, −3, 4, −1, 2, 1, −5, 4}
+ * 其中最大子串为{4，-1，2，1},其和为6
+ */
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void maximumSubarray(int[] array){
+		
+	}
+}
