37dbefaab952df8eaf3a12521814468989f0219a
LinYongW <linyongw@gmail.com>
2014-10-23 10:57:47 +0800

    add dazhong program problems but not to solve

---
+++ WrittenExamination-Interview/src/main/java/cn/campusRecruitment2014/daZhong/FindSortKind/FindSortKind.java
@@ -0,0 +1,24 @@
+package cn.campusRecruitment2014.daZhong.FindSortKind;
+
+/**
+ * 
+ *@explanation 从一个txt文件中，找出特定字符的个数
+ *@author YongLin
+ *@email  yonglin@hengtiansoft.com
+ *@time   2014年10月23日上午10:47:25
+ */
+
+/*
+ * 本题为2014年大众点评校园招聘线上笔试程序题一
+ * 用户每次访问一次服务器，会记录下用户访问的信息，并把信息保存到文件中，该信息格式如下：
+ * 
+ * 其中
+ * 给定一个记录文件，里面的记录数目不大于100000，编写程序找出文件中每一种排序的个数
+ */
+public class FindSortKind {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ WrittenExamination-Interview/src/main/java/cn/campusRecruitment2014/daZhong/MyHashTable/MyHashTable.java
@@ -0,0 +1,17 @@
+package cn.campusRecruitment2014.daZhong.MyHashTable;
+
+/**
+ * 
+ *@explanation 实现哈希表
+ *@author YongLin
+ *@email  yonglin@hengtiansoft.com
+ *@time   2014年10月23日上午10:54:03
+ */
+
+/*
+ * 本题为2014年大众点评校园招聘线上笔试编程题二
+ * 实现哈希表，并给出几种解决冲突的方式
+ */
+public class MyHashTable {
+
+}
